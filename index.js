acd1acd0cbdc245b6f75ce102d8f40db31175be3
Joao Pedro <802.018@alunos.unigran.br>
2020-10-28 11:09:55 -0300

    Exemplo de validacao comcluida

--- index.js
+++ index.js
@@ -6,23 +6,26 @@ npm instal body-parser --save
 npm instal express-session --save
 npm instal ejs --save
 npm instal express-flash --save
+npm install cookie-parser --save
 */
 const bodyParser = require("body-parser");
 var express = require("express");
 const flash = require("express-flash");
 var app = express();
 var session = require("express-session");
+var cookieParser = require("cookie-parser");
 
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+app.use(cookieParser("jsaddsh"));
 app.use(session({
     secret:'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie:{secure:true}
+    cookie:{maxAge: 60000}
 }))
 
 app.use(flash())
@@ -30,10 +33,51 @@ app.use(flash())
 
 //rotas
 app.get("/",(req,res)=>{
-    console.log("Está rodando...");
-    res.send("Rodando...");
+    var emailError =req.flash("emailError");
+    var pontosError=req.flash("pontosError");
+    var nomeError=req.flash("nomeError");
+    var email = req.flash("email");
+
+    emailError = (emailError == undefined || emailError.length == 0) ? undefined : emailError;
+    email = (email == undefined || email.length ==0)?"":email;
+    res.render("index",{emailError,pontosError,nomeError, email: email});
 })
 
+app.post("/form",(req,res)=>{
+    var {email, nome, pontos} = req.body;
+
+    var emailError;
+    var pontosError;
+    var nomeError;
+
+
+    if(email == undefined || email == ""){
+        emailError = "O email não pode ser vazio";
+    }
+
+    if(pontos == undefined || pontos<20){
+      pontosError ="Nao pode ter menos de 20 pontos";
+    }
+
+    if(nome == undefined ||nome == ""){
+        nomeError ="O nome não pode ser vazio";
+    }
+    if(nome.length>4){
+        nomeError ="O nome é muito pequeno";
+    }
+
+    if(emailError != undefined||pontosError!=undefined||nomeError!=undefined){
+        req.flash("emailError",emailError);
+        req.flash("pontosError",pontosError);
+        req.flash("nomeError",nomeError);
+        req.flash("email",email);
+
+        res.redirect("/");
+    }else{
+        res.send("Show de bola");
+    }
+ 
+})
 
 app.listen(5678,(req,res)=>{
     console.log("SERVER RODANDO");
